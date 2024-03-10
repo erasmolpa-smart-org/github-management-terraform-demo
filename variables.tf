99986deae792c0e55e3e6d87c232749dcc25d988
Erasmo <erasmolpa@gmail.com>
2024-03-10 16:14:13 +0000

    pre-commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+
+variable "github_token" {
+  type      = string
+  sensitive = true
+  default   = "gho_XXXXXXX"
+}
+
+variable "github_owner" {
+  type    = string
+  default = "erasmoXXXXXX"
+}
